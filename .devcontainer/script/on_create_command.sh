075e690341682baace1b57799cc1a957c2798342
zen96k <jolly96k@gmail.com>
2026-06-17 06:16:32 +0900

    リポジトリを初期化

---
+++ .devcontainer/script/on_create_command.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+SCRIPT_DIRNAME=$(cd $(dirname ${0}) && pwd)
+PROJECT_DIRNAME=$(cd ${SCRIPT_DIRNAME}/../.. && pwd)
+
+cd ${PROJECT_DIRNAME}
+
+cp -rfv .zshrc ${HOME}/.zshrc
