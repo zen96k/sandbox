647ebea248ec1c1c0abe7d8a5e6185231084b3e5
Kotaro Kuroda <jolly96k@gmail.com>
2026-06-21 19:12:49 +0900

    Update nuxt.config.ts

--- sandbox/nuxt.config.ts
+++ sandbox/nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui"]
+  modules: ["@nuxt/ui"],
+  nitro: {
+    preset: "vercel"
+  }
 })
