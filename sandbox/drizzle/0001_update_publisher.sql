dbfee8f6b0f88b2eabc112b36708f604da968de7
Kotaro Kuroda <jolly96k@gmail.com>
2026-06-22 00:54:23 +0900

    ワークフローを修正

--- sandbox/drizzle/0001_update_publisher.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `publisher` (`name`, `url`) VALUES
-  ('Qiita', 'https://qiita.com/popular-items/feed.atom'),
-  ('Zenn', 'https://zenn.dev/feed');
---
+++ sandbox/server/db/seed/index.ts
@@ -0,0 +1,4 @@
+import "dotenv/config"
+import { seedPublisher } from "./publisher"
+
+await seedPublisher()
