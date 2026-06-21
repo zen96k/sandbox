fbac380bb629b21615b27f021c6e1afb163b76ca
Kotaro Kuroda <jolly96k@gmail.com>
2026-06-22 01:09:17 +0900

    Update relations.ts

--- sandbox/server/db/schema/relations.ts
+++ sandbox/server/db/schema/relations.ts
@@ -2,13 +2,15 @@ import { relations } from "drizzle-orm"
 import { article } from "./article"
 import { publisher } from "./publisher"
 
-export const publisherRelations = relations(publisher, ({ many }) => ({
-  article: many(article)
-}))
+export const publisherRelations = relations(publisher, ({ many }) => {
+  return { article: many(article) }
+})
 
-export const articleRelations = relations(article, ({ one }) => ({
-  publisher: one(publisher, {
-    fields: [article.publisherId],
-    references: [publisher.id]
-  })
-}))
+export const articleRelations = relations(article, ({ one }) => {
+  return {
+    publisher: one(publisher, {
+      fields: [article.publisherId],
+      references: [publisher.id]
+    })
+  }
+})
