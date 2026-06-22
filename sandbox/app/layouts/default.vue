f641fd69c07bd0c9b762771f385683d5cd292b0f
zen96k <jolly96k@gmail.com>
2026-06-23 01:01:41 +0900

    アプリ名称をヘッダーに表示

--- sandbox/app/layouts/default.vue
+++ sandbox/app/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <header class="border-b border-gray-200 dark:border-gray-800">
-      <UContainer class="flex items-center justify-end py-3">
+      <UContainer class="flex items-center justify-between py-3">
+        <span class="text-2xl font-semibold">Sandbox</span>
         <UColorModeButton />
       </UContainer>
     </header>
