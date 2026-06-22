24ac15360456cdea53363ada8dc136cab6fcfd8e
zen96k <jolly96k@gmail.com>
2026-06-23 00:57:00 +0900

    カラーモードを選択できる

---
+++ sandbox/app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header class="border-b border-gray-200 dark:border-gray-800">
+      <UContainer class="flex items-center justify-end py-3">
+        <UColorModeButton />
+      </UContainer>
+    </header>
+    <slot />
+  </div>
+</template>
